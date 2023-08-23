46f5dac357852377a23191c512500124be847c89
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-08-24 01:58:24 +0600

    added new api endpoint

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'https://rtk-chat-app.onrender.com/api',
+		baseUrl: 'https://express-app-8plc.onrender.com/api',
 		credentials: 'include'
 	}),
 	endpoints: () => ({})
